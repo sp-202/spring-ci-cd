c90ce2bd8bd5bdb5a28295732ac5a16e8860c41b
Subhodeep <subhodeeppal64@gmail.com>
2022-06-05 20:23:00 +0530

    controller added

---
+++ src/test/java/com/example/prac02/Prac02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
